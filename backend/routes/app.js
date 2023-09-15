be4397d06012dc747fb258db564ab3112a919b77
Davth5 <davth5@gmail.com>
2023-09-15 10:21:42 -0600

    minor user.js route changes

--- backend/routes/app.js
+++ backend/routes/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const knex = require("knex")(
-  require("./knexfile.js")[process.env.NODE_ENV || "development"]
+  require("../knexfile.js")[process.env.NODE_ENV || "development"]
 );
 app.use(express.json());
 
