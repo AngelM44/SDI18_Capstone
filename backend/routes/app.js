805d1ec6e6d3678c85d87083f166d1a4c3bbf822
Davth5 <davth5@gmail.com>
2023-09-15 10:04:38 -0600

    Working in Express API; created routes

---
+++ backend/routes/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const knex = require("knex")(
+  require("./knexfile.js")[process.env.NODE_ENV || "development"]
+);
+app.use(express.json());
+
+app.use(cors);
+
+app.use("/users", usersRoutes);
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
