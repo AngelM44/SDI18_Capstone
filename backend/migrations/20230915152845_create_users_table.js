c522cf5a44ac6d0d07da17383ca37285e0bc59da
Davth5 <davth5@gmail.com>
2023-09-15 13:41:14 -0600

    Removed id from faker; not needed as it auto increments; crud functions for users checks out

--- backend/migrations/20230915152845_create_users_table.js
+++ backend/migrations/20230915152845_create_users_table.js
@@ -4,7 +4,7 @@
  */
 exports.up = function(knex) {
   return knex.schema.createTable('users', table => {
-      table.increments('id')
+      table.increments('id');
       table.string('first_name', 255).notNullable()
       table.string('last_name', 255).notNullable()
       table.string('dod_id', 10).notNullable()
