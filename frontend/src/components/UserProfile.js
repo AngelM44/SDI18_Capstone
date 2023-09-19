b1de919a4578ed30d7eb36726344c6f6b247e523
Patrick <pschilber@gmail.com>
2023-09-19 16:07:14 -0400

    created dynamic profile routes and rendering

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { EuiFlexGroup } from "@elastic/eui";
 import UserProfilePanel from "./UserProfilePanel";
 import UserInfo from "./UserInfo";
 import UserProfileLoader from "./UserProfileLoader";
 
 const UserProfile = () => {
-  return (
-    <EuiFlexGroup style={{ marginLeft: "5px", marginRight: "5px" }}>
-      <div style={{ width: "30%" }}>
-        <UserProfilePanel />
-      </div>
-      <div style={{ width: "70%" }}>
-        <UserInfo />
-      </div>
-    </EuiFlexGroup>
-  );
+  const { id } = useParams();
+  const [profileData, setProfileData] = useState([]);
+  // const [selected, setSelected] = useState(false);
+  // const { reservation, setReservation } = useVehicleContext();
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/profile/${id}`)
+      .then((response) => response.json())
+      .then((data) => setProfileData(data))
+      .catch((error) => console.error(error));
+  }, [id]);
+
+  if (profileData.id === undefined) {
+    return <UserProfileLoader />;
+  } else {
+    return (
+      <EuiFlexGroup style={{ marginLeft: "5px", marginRight: "5px" }}>
+        {/* {console.log(profileData)} */}
+        <div style={{ width: "30%" }}>
+          <UserProfilePanel data={profileData} />
+        </div>
+        <div style={{ width: "70%" }}>
+          <UserInfo data={profileData} />
+        </div>
+      </EuiFlexGroup>
+    );
+  }
 };
 
 export default UserProfile;
--- frontend/src/components/UserProfilePanel.js
+++ frontend/src/components/UserProfilePanel.js
@@ -1,43 +1,45 @@
-import React from "react";
 import { EuiCard, EuiDescriptionList } from "@elastic/eui";
 
-const data = [
-  {
-    title: "Location:",
-    description: "Base Name",
-  },
-  {
-    title: "Availibility:",
-    description: "Info on schedule",
-  },
-  {
-    title: "Contact Info:",
-    description: "Email Link",
-  },
-  {
-    title: "Social Media:",
-    description: "Filler links/Icons",
-  },
-];
+const UserProfilePanel = ({ data }) => {
+  // const [profileData, setProfileData] = useState(props);
 
-const cardFooterContent = (
-  <EuiDescriptionList
-    type="column"
-    listItems={data}
-    // style={{ maxWidth: "420px" }}
-  />
-);
+  const cardFooterContent = (
+    <EuiDescriptionList
+      type="column"
+      listItems={[
+        {
+          title: "Location:",
+          description: `${data.location}`,
+        },
+        {
+          title: "Availibility:",
+          description: `${data.availability}`,
+        },
+        {
+          title: "Contact Info:",
+          description: `${data.email}`,
+        },
+        // {
+        //   title: "Social Media:",
+        //   description: "Filler links/Icons",
+        // },
+      ]}
+      // style={{ maxWidth: "420px" }}
+    />
+  );
 
-const UserProfilePanel = () => (
-  <EuiCard
-    style={{ marginTop: "10px" }}
-    textAlign="left"
-    // href="https://elastic.github.io/eui/"
-    image="https://source.unsplash.com/532x532/?City"
-    // icon={<EuiIcon size="xxl" type="logoBeats" />}
-    title={"First Name Last Name"}
-    footer={cardFooterContent}
-  />
-);
+  return (
+    <>
+      <EuiCard
+        style={{ marginTop: "10px" }}
+        textAlign="left"
+        // href="https://elastic.github.io/eui/"
+        image="../profile_pics/profile-pic5.png"
+        title={`${data.first_name} ${data.last_name}`}
+        footer={cardFooterContent}
+      />
+    </>
+  );
+};
 
 export default UserProfilePanel;
