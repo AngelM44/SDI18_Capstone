03bff6d80d8f871869319a27c2dce456b2030c5b
angel <angel.mora.1@outlook.com>
2023-09-20 09:45:56 -0700

    backend updated to new ERD and seeds updated

--- backend/seeds/4_interests.js
+++ backend/seeds/4_interests.js
@@ -7,15 +7,58 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('interests').del()
 
-  let interests = []
-
-  for(let i = 1; i <= 20; i++){
-    interests.push({
-      name: faker.random.arrayElement(["Weightlifting", "Swimming", "Basketball", "Soccer", "Crossfit", "Surfing", "Snowboarding", "Skiing"]),
-      skill_level: faker.random.arrayElement(["Beginner", "Intermidiate", "Advanced"]),
-      category: faker.random.arrayElement(["Cardio", "Weightlifting"]),
-    })
+  let interests = [
+  {
+    name: 'Weightlifting',
+    skill_level: 'beginner',
+    category: 'Strength Training'
+  },
+  {
+    name: 'Crossfit',
+    skill_level: 'beginner',
+    category: 'Cardiovascular and Strength Training'
+  },
+  {
+    name: 'Running',
+    skill_level: 'intermediate',
+    category: 'Cardiovascualar Training'
+  },
+  {
+    name: 'Yoga',
+    skill_level: 'advanced',
+    category: 'Flexibility and Endurance'
+  },
+  {
+    name: 'High-Intensity Interval Training',
+    skill_level: 'intermediate',
+    category: 'Cardiovascular and Endurance'
+  },
+  {
+    name: 'Olympic Lifting',
+    skill_level: 'advanced',
+    category: 'Strength Training and Endurance'
+  },
+  {
+    name: 'Swimming',
+    skill_level: 'intermediate',
+    category: 'Cardiovascular and Endurance'
+  },
+  {
+    name: 'Dance Classes',
+    skill_level: 'advanced',
+    category: 'Cardiovascular and Endurance'
+  },
+  {
+    name: 'Squats',
+    skill_level: 'beginner',
+    category: 'Strength Training'
+  },
+  {
+    name: 'Deadlift',
+    skill_level: 'advanced',
+    category: 'Strength Training'
   }
+]
 
   await knex('interests').insert(interests);
 };
