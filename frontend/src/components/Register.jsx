491ee1be9fd87c50daaa3c3a1b20168c77c3e6aa
Davth5 <davth5@gmail.com>
2023-09-20 08:35:42 -0600

    Additional styling on Register

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -58,84 +58,130 @@ function Register() {
   };
 
   return (
-    <EuiPage>
-      <EuiPageBody>
-        <EuiTitle size="l">
-          <h2>Register</h2>
-        </EuiTitle>
-        <EuiSpacer size="l" />
-        <EuiForm component="form" onSubmit={handleSubmit}>
-          <EuiFormRow label="First Name">
-            <EuiFieldText
-              name="first_name"
-              value={formData.first_name}
-              onChange={handleChange}
-              required
-            />
-          </EuiFormRow>
-          <EuiFormRow label="Last Name">
-            <EuiFieldText
-              name="last_name"
-              value={formData.last_name}
-              onChange={handleChange}
-              required
-            />
-          </EuiFormRow>
-          <EuiFormRow label="DOD ID">
-            <EuiFieldText
-              name="dod_id"
-              value={formData.dod_id}
-              onChange={handleChange}
-              required
-            />
-          </EuiFormRow>
-          <EuiFormRow label="Username">
-            <EuiFieldText
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              required
-            />
-          </EuiFormRow>
-          <EuiFormRow label="Password">
-            <EuiFieldText
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-          </EuiFormRow>
-          <EuiFormRow label="Email">
-            <EuiFieldText
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </EuiFormRow>
-          <EuiFormRow label="Location">
-            <EuiFieldText
-              name="location"
-              value={formData.location}
-              onChange={handleChange}
-              required
-            />
-          </EuiFormRow>
-          <EuiSpacer />
-          <EuiButton type="submit">Register</EuiButton>
-          {successMessage && (
-            <EuiCallOut title={successMessage} color="success" />
-          )}
-          {error && (
-            <EuiCallOut title="Error" color="danger">
-              {error}
-            </EuiCallOut>
-          )}
-        </EuiForm>
+    <EuiPage style={styles.page}>
+      <EuiPageBody style={styles.container}>
+        <div style={styles.formContainer}>
+          <EuiTitle size="l">
+            <h2>Register</h2>
+          </EuiTitle>
+          <EuiSpacer size="l" />
+          <EuiForm component="form" onSubmit={handleSubmit} style={styles.form}>
+            <EuiFormRow style={styles.formRow} label="First Name">
+              <EuiFieldText
+                name="first_name"
+                value={formData.first_name}
+                onChange={handleChange}
+                required
+              />
+            </EuiFormRow>
+            <EuiFormRow style={styles.formRow} label="Last Name">
+              <EuiFieldText
+                name="last_name"
+                value={formData.last_name}
+                onChange={handleChange}
+                required
+              />
+            </EuiFormRow>
+            <EuiFormRow style={styles.formRow} label="DOD ID">
+              <EuiFieldText
+                name="dod_id"
+                value={formData.dod_id}
+                onChange={handleChange}
+                required
+              />
+            </EuiFormRow>
+            <EuiFormRow style={styles.formRow} label="Username">
+              <EuiFieldText
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+              />
+            </EuiFormRow>
+            <EuiFormRow style={styles.formRow} label="Password">
+              <EuiFieldText
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </EuiFormRow>
+            <EuiFormRow style={styles.formRow} label="Email">
+              <EuiFieldText
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </EuiFormRow>
+            <EuiFormRow style={styles.formRow} label="Location">
+              <EuiFieldText
+                name="location"
+                value={formData.location}
+                onChange={handleChange}
+                required
+              />
+            </EuiFormRow>
+            <EuiSpacer />
+            <div style={styles.buttonContainer}>
+              <EuiButton type="submit">Register</EuiButton>
+            </div>
+            {successMessage && (
+              <EuiCallOut title={successMessage} color="success" />
+            )}
+            {error && (
+              <EuiCallOut title="Error" color="danger">
+                {error}
+              </EuiCallOut>
+            )}
+          </EuiForm>
+        </div>
       </EuiPageBody>
     </EuiPage>
   );
 }
+
+const styles = {
+  page: {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    background: "#f3f3f3",
+    marginTop: "60px",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  formContainer: {
+    width: "80%",
+    maxWidth: "600px",
+    padding: "20px",
+    borderRadius: "10px",
+    boxShadow: "0px 0px 15px rgba(0, 0, 0, 0.1)",
+    background: "white",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%",
+  },
+  formRow: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+    width: "100%",
+    marginTop: "20px",
+  },
+};
 
 export default Register;
