6d30c552fdafba412d121d8448168db36020d770
Davth5 <davth5@gmail.com>
2023-09-21 10:44:15 -0600

    Home Page works with interests rendered; also changed unsplash to random ids

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,106 +1,127 @@
-
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
-import {
-  EuiButton,
-  EuiCard,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiIcon,
-  EuiFlexGrid
-} from "@elastic/eui";
-import './App.css';
+import { Link } from "react-router-dom";
+import { EuiCard, EuiFlexGroup, EuiFlexItem, EuiFlexGrid } from "@elastic/eui";
+import "./App.css";
 
 function Home() {
-  const [users, setUsers] = useState([]);
-  const [profiles, setProfiles] = useState([]);
-  const [interests, setInterests] = useState([]);
   const [combinedData, setCombinedData] = useState([]);
 
+  const fetchInterestsForUser = async (interestIds) => {
+    try {
+      const fetchedInterests = await Promise.all(
+        interestIds.map((interestId) =>
+          fetch(`http://localhost:8080/interests/${interestId}`).then((res) =>
+            res.json()
+          )
+        )
+      );
+      return fetchedInterests.map((interest) => interest.name);
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
+
   useEffect(() => {
-    // Fetch users
-    fetch("http://localhost:8080/users")
-      .then(response => response.json())
-      .then(data => {
-        setUsers(data);
-        // Fetch profiles for all users
-        return Promise.all(data.map(user =>
-          fetch(`http://localhost:8080/profile/${user.id}`)
-            .then(response => response.json())));
-      })
-      .then(profileData => setProfiles(profileData))
-      .catch(error => console.error("Error fetching users and profiles:", error));
-    // Fetch interests
-    fetch("http://localhost:8080/interests")
-      .then(response => response.json())
-      .then(data => setInterests(data))
-      .catch(error => console.error("Error fetching interests:", error));
+    const fetchData = async () => {
+      try {
+        const usersResponse = await fetch("http://localhost:8080/users");
+        const users = await usersResponse.json();
+
+        const profilesResponse = await Promise.all(
+          users.map((user) =>
+            fetch(`http://localhost:8080/profile/${user.id}`).then((response) =>
+              response.json()
+            )
+          )
+        );
+
+        const dataWithInterests = await Promise.all(
+          users.map(async (user, index) => {
+            const userProfile = profilesResponse[index];
+            const userInterests = await fetchInterestsForUser(
+              userProfile.interests || []
+            );
+            return {
+              ...user,
+              ...userProfile,
+              interests: userInterests,
+            };
+          })
+        );
+
+        setCombinedData(dataWithInterests);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  useEffect(() => {
-    const data = users.map(user => {
-
-      const userProfile = profiles.find(profile => profile.user_id === user.id);
-      return {
-        ...user,
-        interests: ['basketball'], // For now since we can't fetch user_interests
-        profile_pic: userProfile ? userProfile.profile_pic : null // Set default image if not found
-      };
-    });
-    setCombinedData(data);
-  }, [users, profiles, interests]);
-
   return (
-    <div style={{
-      height: '100vh',
-      width: '100vw',
-      minHeight: '100vh',
-      // color: 'white'
-    }}>
+    <div style={{ height: "100vh", width: "100vw", minHeight: "100vh" }}>
       <h1>Users</h1>
       <EuiFlexGrid className="custom-flex-grid" columns={4} gutterSize="s">
-        {combinedData.map(user => (
-          <EuiFlexItem className="custom-flex-item" key={user.id} style={{ padding: "5px" }}>
-            <Link to={`/profile/${user.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-
-              <EuiCard className="euiCard"
+        {combinedData.map((user) => (
+          <EuiFlexItem
+            className="custom-flex-item"
+            key={user.id}
+            style={{ padding: "5px" }}
+          >
+            <Link
+              to={`/profile/${user.id}`}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <EuiCard
+                className="euiCard"
                 style={{
                   marginTop: "10px",
                   minWidth: "300px",
                   maxWidth: "400px",
                   minHeight: "450px",
-                  backgroundColor: '#4267B2',
-                  color: 'white',
-                  borderColor: 'grey',
-                  borderWidth: '1px',
-                  borderStyle: 'solid'
-
+                  backgroundColor: "#4267B2",
+                  color: "white",
+                  borderColor: "grey",
+                  borderWidth: "1px",
+                  borderStyle: "solid",
                 }}
                 textAlign="left"
                 image={
-                  <div style={{
-                    height: '300px',
-                    backgroundSize: 'contain',
-                    backgroundImage: `url(${user.profile_pic ? user.profile_pic : "https://source.unsplash.com/400x200/?User"})`,
-                    backgroundPosition: 'center center',
-                    backgroundRepeat: 'no-repeat'
-                  }}></div>
+                  <div
+                    style={{
+                      height: "300px",
+                      backgroundSize: "contain",
+                      backgroundImage: `url(${
+                        user.profile_pic ||
+                        `https://source.unsplash.com/400x200/?person,portrait&${user.id}`
+                      })`,
+                      backgroundPosition: "center center",
+                      backgroundRepeat: "no-repeat",
+                    }}
+                  ></div>
                 }
                 paddingSize="l"
               >
-                <div style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: '100%',
-                  marginTop: '80px'
-                }}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    height: "100%",
+                    marginTop: "80px",
+                  }}
+                >
                   <h2>{`${user.first_name} ${user.last_name}`}</h2>
-                  <div style={{ textAlign: 'left', flexGrow: 1, marginBottom: '15px' }}>
+                  <div
+                    style={{
+                      textAlign: "left",
+                      flexGrow: 1,
+                      marginBottom: "15px",
+                    }}
+                  >
                     <strong>Email:</strong> {user.email} <br />
                     <strong>Location:</strong> {user.location} <br />
-                    <strong>Interests:</strong> {user.interests.join(', ')}
-                  </div>
-                  <div style={{ textAlign: 'right' }}>
+                    <strong>Interests:</strong> {user.interests.join(", ")}
                   </div>
                 </div>
               </EuiCard>
