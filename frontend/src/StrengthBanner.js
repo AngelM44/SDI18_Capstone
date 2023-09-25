20c62033344248aa0f2fdaf1636668c185a17c3e
codewithchewy <srepp@twu.edu>
2023-09-25 14:22:17 -0600

    added banners to the cardio and strength components

---
+++ frontend/src/StrengthBanner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function StrengthBanner() {
+    return (
+        <div className="strength-banner">
+            <div className="strength-quote">
+                Forge connections, build strength together.
+            </div>
+        </div>
+    );
+}
+
+export default StrengthBanner;
