b591f25347f5139ef79e18cb76b01c03a3f1387e
Patrick <pschilber@gmail.com>
2023-09-26 16:43:35 -0400

    got posts working, and styled

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -26,7 +26,6 @@ router.post("/", async (req, res) => {
   const { profile_id, date_created, body, first_name, last_name } = req.body;
 
   try {
-
     const newPost = await knex("posts")
       .insert({
         profile_id,
@@ -35,16 +34,12 @@ router.post("/", async (req, res) => {
       })
       .returning("*");
 
-
     const updatedProfile = await knex("profile")
       .where({ user_id: profile_id })
-      .update({
-
-      })
+      .update({})
       .returning("*");
 
     if (newPost[0] && updatedProfile[0]) {
-
       res.status(201).json({
         newPost: {
           id: newPost[0].id,
@@ -65,9 +60,6 @@ router.post("/", async (req, res) => {
   }
 });
 
-
-
-
 router.get("/:profileId", async (req, res) => {
   try {
     const profile = await knex
@@ -86,6 +78,16 @@ router.get("/:profileId", async (req, res) => {
   } catch (err) {
     console.error("Error fetching profile:", err.message);
     res.status(400).json("Error fetching profile.");
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await knex("profile").select();
+    res.json(profiles);
+  } catch (err) {
+    console.error("Error fetching users:", err.message);
+    res.status(400).json("Error fetching profiles.");
   }
 });
 
