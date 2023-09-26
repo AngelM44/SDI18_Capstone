272eebe491fe1e9436835ed26780926b275e82d5
Davth5 <davth5@gmail.com>
2023-09-26 15:10:11 -0600

    Styled Userinfo a bit

--- frontend/src/components/UserInfo.js
+++ frontend/src/components/UserInfo.js
@@ -3,6 +3,11 @@ import {
   EuiSpacer,
   EuiHorizontalRule,
   EuiPageBody,
+  EuiPanel,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiTitle,
+  EuiIcon,
 } from "@elastic/eui";
 import React from "react";
 import UserInterests from "./UserInterests";
@@ -10,18 +15,40 @@ import UserPosts from "./UserPosts";
 
 const UserInfo = ({ data }) => (
   <EuiPageBody>
-    <EuiText>
-      <h1>#About Me</h1>
-      <p>{data.info}</p>
-      <EuiHorizontalRule margin="m" size="l" />
-      <h1>#Goals</h1>
-      <p>{data.goals}</p>
-    </EuiText>
-    <EuiHorizontalRule margin="m" />
+    <EuiPanel paddingSize="l" hasShadow>
+      <EuiFlexGroup alignItems="center">
+        <EuiFlexItem grow={false}>
+          <EuiIcon type="user" size="xl" />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiTitle>
+            <h1>About Me</h1>
+          </EuiTitle>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiText>
+        <p>{data.info}</p>
+      </EuiText>
+      <EuiHorizontalRule margin="m" />
+      <EuiFlexGroup alignItems="center">
+        <EuiFlexItem grow={false}>
+          <EuiIcon type="sparkles" size="xl" />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiTitle>
+            <h1>Goals</h1>
+          </EuiTitle>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiText>
+        <p>{data.goals}</p>
+      </EuiText>
+    </EuiPanel>
+    <EuiSpacer size="m" />
     <UserInterests data={data} />
     <EuiHorizontalRule margin="m" />
     <UserPosts data={data} />
-    <EuiSpacer />
+    <EuiSpacer size="m" />
   </EuiPageBody>
 );
 
