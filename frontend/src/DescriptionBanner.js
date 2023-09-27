735fd2889b9e653e83fbf32ba94661b324ab0691
codewithchewy <srepp@twu.edu>
2023-09-27 08:45:25 -0600

    changed name to MyFitConnect and rounded out the text backgrounds

--- frontend/src/DescriptionBanner.js
+++ frontend/src/DescriptionBanner.js
@@ -6,7 +6,7 @@ function DescriptionBanner() {
         <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1 description-banner">
             <div className="circle-container">
                 <div className="circle-text">
-                    <h1>Welcome to FitConnect</h1>
+                    <h1>Welcome to MyFitConnect</h1>
                     <p>Your go-to platform to connect with others in your base and stay on track with your fitness goals.</p>
                 </div>
             </div>
