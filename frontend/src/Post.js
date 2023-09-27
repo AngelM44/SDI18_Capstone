7bfd5fd628081dff4daebe6d427487583ab84d23
Patrick <pschilber@gmail.com>
2023-09-27 11:16:57 -0400

    updated styling on buttons

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -1,56 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { EuiButton, EuiTextArea, EuiFormRow } from "@elastic/eui";
 
 function Post({ onNewPost, data }) {
-    const [newPostContent, setNewPostContent] = useState('');
+  const [newPostContent, setNewPostContent] = useState("");
 
-    const handleNewPost = async () => {
-        console.log("Attempting to create a new post");
-        console.log("Profile ID being passed:", data.profile_id);
+  const handleNewPost = async () => {
+    console.log("Attempting to create a new post");
+    console.log("Profile ID being passed:", data.profile_id);
 
-        const date_created = new Date().toISOString().slice(0, 10); // Current date in 'YYYY-MM-DD' format
-        try {
-            const response = await fetch('http://localhost:8080/posts', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    body: newPostContent,
+    const date_created = new Date().toISOString().slice(0, 10); // Current date in 'YYYY-MM-DD' format
+    try {
+      const response = await fetch("http://localhost:8080/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          body: newPostContent,
 
-                    profile_id: data.profile_id, // This is a placeholder. Will replace it with the actual user's profile_id
-                    date_created: date_created // Current date in 'YYYY-MM-DD' format
+          profile_id: data.profile_id, // This is a placeholder. Will replace it with the actual user's profile_id
+          date_created: date_created, // Current date in 'YYYY-MM-DD' format
+        }),
+      });
 
-                }),
-            });
+      const result = await response.json();
 
-            const result = await response.json();
+      if (response.ok) {
+        console.log("Post created:", result);
+        onNewPost(result);
+        setNewPostContent("");
+      } else {
+        console.error("Error creating post:", result.error);
+      }
+    } catch (error) {
+      console.error("Error creating post:", error);
+    }
+  };
 
-            if (response.ok) {
-                console.log('Post created:', result);
-                onNewPost(result);
-                setNewPostContent('');
-            } else {
-                console.error('Error creating post:', result.error);
-            }
-        } catch (error) {
-            console.error('Error creating post:', error);
-        }
-    };
-
-    return (
-        <div className="post-container">
-            <textarea
-                className="post-textarea"
-                value={newPostContent}
-                onChange={(e) => setNewPostContent(e.target.value)}
-                placeholder="What's on your mind?"
-            />
-            <EuiButton className="post-button" onClick={handleNewPost}>Post</EuiButton>
-        </div>
-    );
+  return (
+    <div className="post-container">
+      <textarea
+        className="post-textarea"
+        value={newPostContent}
+        onChange={(e) => setNewPostContent(e.target.value)}
+        placeholder="What's on your mind?"
+      />
+      <EuiButton
+        size="s"
+        color="secondary"
+        className="post-button"
+        onClick={handleNewPost}
+      >
+        Post
+      </EuiButton>
+    </div>
+  );
 }
 
 export default Post;
-
-
--- frontend/src/components/SearchResults.jsx
+++ frontend/src/components/SearchResults.jsx
@@ -126,7 +126,12 @@ const SearchResults = ({ results, onClose }) => {
               </EuiText>
             ))}
             {showLoadMore && (
-              <EuiButton size="s" onClick={loadMore}>
+              <EuiButton
+                color="secondary"
+                size="xxs"
+                style={{ width: "20%" }}
+                onClick={loadMore}
+              >
                 Load More
               </EuiButton>
             )}
